84b3849818aeb11c5d8253c9046b02df87eebc60
lsp2610 <louis.porter26@gmail.com>
2025-03-04 07:23:43 +0000

    added bayesian model with resim

--- data/fetch_match_data.py
+++ data/fetch_match_data.py
@@ -1,7 +1,7 @@
 import sqlite3
 import pandas as pd
 
-def load_data():
+def load_data(days_ago=365):
     """
     Load shot data from the database and create match summaries.
     
@@ -15,6 +15,10 @@ def load_data():
     shot_data = pd.read_sql_query("SELECT * FROM prem_data", conn)
     conn.close()
     
+    # Filter data to only include shots from the last year
+    shot_data['match_date'] = pd.to_datetime(shot_data['match_date'])
+    shot_data = shot_data[shot_data['match_date'] > pd.Timestamp('today') - pd.Timedelta(days=days_ago)]
+
     # Add a goal column
     shot_data['is_goal'] = shot_data['Outcome'].apply(lambda x: 1 if x == 'Goal' else 0)
     
@@ -23,14 +27,14 @@ def load_data():
     away_shots = shot_data[shot_data['Team'] == shot_data['away_team']]
     
     # Aggregate by match for home teams
-    home_stats = home_shots.groupby(['match_url', 'match_date', 'home_team', 'away_team', 'season'], as_index=False).agg({
+    home_stats = home_shots.groupby(['match_url', 'match_date', 'home_team', 'away_team', 'season', 'division'], as_index=False).agg({
         'is_goal': 'sum',  # Total goals
         'xG': 'sum',       # Total xG
         'PSxG': 'sum'      # Total PSxG
     })
     
     # Aggregate by match for away teams
-    away_stats = away_shots.groupby(['match_url', 'match_date', 'home_team', 'away_team', 'season'], as_index=False).agg({
+    away_stats = away_shots.groupby(['match_url', 'match_date', 'home_team', 'away_team', 'season', 'division'], as_index=False).agg({
         'is_goal': 'sum',  # Total goals
         'xG': 'sum',       # Total xG
         'PSxG': 'sum'      # Total PSxG
@@ -53,7 +57,7 @@ def load_data():
     match_summaries = pd.merge(
         home_stats, 
         away_stats, 
-        on=['match_url', 'match_date', 'home_team', 'away_team', 'season'],
+        on=['match_url', 'match_date', 'home_team', 'away_team', 'season', "division"],
         how='inner'
     )
     
